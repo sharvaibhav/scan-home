a30e411a7d6afd6edd166976b9f2e871abefe61a
Vaibhav Sharma <vaibhav.sharma@aize.io>
2023-05-04 01:19:06 +0200

    first commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,16 @@
+import { api } from "~/utils/api";
+
+import "~/styles/globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+
+import type { AppProps } from "next/app";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ClerkProvider {...pageProps}>
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
+}
+
+export default api.withTRPC(MyApp);
